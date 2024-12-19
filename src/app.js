ef660ea7a61469ca2c4a3882f4c9dc8ed9ba6104
Orlando Murillo <j.orlandomurillo@gmail.com>
2024-12-19 15:14:53 -0800

    feat: create appointment route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const appointmentRoutes = require("./routes/appointment");
 
 const app = express();
 app.use(express.json());
+
+// Register routes
+app.use("/api/appointments", appointmentRoutes);
 
 const PORT = process.env.PORT || 3000;
 
