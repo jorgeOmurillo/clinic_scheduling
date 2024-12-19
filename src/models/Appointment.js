017a4d333b9df682f45d1f1d83360f5c47cf3223
Orlando Murillo <j.orlandomurillo@gmail.com>
2024-12-19 15:07:06 -0800

    feat: create Appointment model

---
+++ src/models/Appointment.js
@@ -0,0 +1,34 @@
+class Appointment {
+  static TYPES = {
+    INITIAL_CONSULTATION: { name: "initial", duration: 90 },
+    STANDARD: { name: "standard", duration: 60 },
+    CHECK_IN: { name: "checkin", duration: 30 },
+  };
+
+  constructor(type, startTime) {
+    if (!Object.values(Appointment.TYPES).find((t) => t.name === type)) {
+      throw new Error("Invalid appointment type");
+    }
+    this.type = type;
+    this.startTime = new Date(startTime);
+    this.duration = this.getDuration();
+    this.endTime = this.calculateEndTime();
+  }
+
+  getDuration() {
+    return Object.values(Appointment.TYPES).find((t) => t.name === this.type)
+      .duration;
+  }
+
+  calculateEndTime() {
+    const endTime = new Date(this.startTime);
+    endTime.setMinutes(endTime.getMinutes() + this.duration);
+    return endTime;
+  }
+
+  overlaps(other) {
+    return this.startTime < other.endTime && this.endTime > other.startTime;
+  }
+}
+
+module.exports = Appointment;
